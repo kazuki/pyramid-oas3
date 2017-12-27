87e5d4027687c3e9f0da5ba21672724bbde6f57e
Kazuki Oikawa <k@oikw.org>
2017-12-27 23:59:09 +0900

    0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _load_lines(filename):
 
 setup(
     name='pyramid_oas3',
-    version='0.0.3',
+    version='0.0.4',
     description='OpenAPI 3.0 Validator for Pyramid',
     packages=['pyramid_oas3', 'pyramid_oas3.jsonschema'],
     author='Kazuki Oikawa',
